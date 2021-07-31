0a07c87d11e311618f069f112adf672ffb5b88f7
awal-ahmed <50808571+awal-ahmed@users.noreply.github.com>
2021-08-01 04:02:15 +0600

    gpu commant

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from kapre.composed import get_melspectrogram_layer
 import tensorflow as tf
 
 physical_devices = tf.config.list_physical_devices('GPU') 
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#tf.config.experimental.set_memory_growth(physical_devices[0], True)
 import os
 
 
